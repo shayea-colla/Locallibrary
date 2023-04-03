4005691350166f187e104effb2c89914ac49f709
samy <samysam204o@gmail.com>
2023-04-03 11:26:53 +0300

    added the permission field to book instance

--- catalog/models.py
+++ catalog/models.py
@@ -80,6 +80,7 @@ class BookInstance(models.Model):
 
     class Meta:
         ordering = ["due_back"]
+        permissions = (("can mark as returned", "set book as returned"),)
 
 
     def __str__(self):
